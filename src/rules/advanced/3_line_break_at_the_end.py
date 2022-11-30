61c9ed8670397f19a697c18749d6a98cccf7c7b3
sigmanificient <edhyjox@gmail.com>
2022-11-30 20:43:43 +0100

    :label: Fix typing

--- src/rules/advanced/3_line_break_at_the_end.py
+++ src/rules/advanced/3_line_break_at_the_end.py
@@ -1,10 +1,16 @@
-from wera import FileType, File, rule
+from wera import FileType, rule
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from wera import File
+    from typing import Iterator, Tuple
 
 
 @rule(
     description="A-3: Missing line break at end of file",
     type_filter=FileType.project()
 )
-def check(file: File):
+def check(file: File) -> Iterator[Tuple[File, int]]:
     if file.lines[-1]:
         yield file, len(file.lines)
