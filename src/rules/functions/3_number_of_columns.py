37124e409b2d9f1531cf0b6fd5c1f427b6135f91
sigmanificient <edhyjox@gmail.com>
2022-11-30 22:22:53 +0100

    :sparkles: Add rule C-F3

---
+++ src/rules/functions/3_number_of_columns.py
@@ -0,0 +1,24 @@
+from __future__ import annotations
+
+from wera import FileType, rule
+
+from typing import TYPE_CHECKING
+
+if TYPE_CHECKING:
+    from wera import File
+    from typing import Final, Iterator, Tuple
+
+
+TAB_SIZE: Final[int] = 4
+
+
+@rule(
+    description="(Major) F-3: Exceeding the 80 column limit",
+    type_filter=FileType.source()
+)
+def check(file: File) -> Iterator[Tuple[File, int]]:
+    for idx, line in enumerate(file.lines):
+        line_no_tab = line.replace('\t', ' ' * TAB_SIZE)
+
+        if len(line_no_tab) > 80:
+            yield file, idx
